a76e7d175b11055e08db621ba8e1330b6540717f
root <root@localhost.localdomain>
2018-12-04 06:37:04 +0530

    commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# authtut" 
+1.go to client folder and from terminal run command to save sessionid in text file
+  curl -X GET http://localhost:3000 -v -c cookie-file.txt
+2.to send the saved session id in request header
+  curl -X GET http://localhost:3000 -v -b cookie-file.txt
--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 //npm modules
 const express = require('express');
+const session = require('express-session');
+const uuid = require('uuid/v4');
+const FileStore = require('session-file-store')(session);
 
 // create the server
 const app = express();
 
+// add & configure middleware
+app.use(session({
+  genid: (req) => {
+    console.log('Inside the session middleware')
+    console.log(req.sessionID)
+    return uuid() // use UUIDs for session IDs
+  },
+  secret: 'keyboard cat',
+  resave: false,
+  store: new FileStore(),
+  saveUninitialized: true
+}));
+
 // create the homepage route at '/'
 app.get('/', (req, res) => {
-  res.send('you just hit the home page\n');
+  console.log('Inside the homepage callback function');
+  console.log(req.sessionID);
+  res.send(`You hit home page!\n`);
 });
 
 // tell the server what port to listen on
